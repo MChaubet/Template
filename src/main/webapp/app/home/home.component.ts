7bec55a555244fcb05239d6ff5b62e9e3eafb335
MChaubet <mathieu.chaubet.travail@gmail.com>
2022-03-17 23:50:33 +0100

    [FRONT] : Mise en place du footer avec description entreprise + logo + pricing

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -14,6 +14,27 @@ import { Account } from 'app/core/auth/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
 
+  possibilities: { name: string; desc: string; pricing: string }[] = [
+    {
+      name: 'Site vitrine',
+      desc: "Création d'un site vitrine pour artisan, description de l'activité et des produits",
+      pricing: '750€ - 1500€',
+    },
+    {
+      name: 'Site vitrine + Réservation/Connexion',
+      desc: "Création d'un site vitrine pour artisan, description de l'activité et des produits + mise en place d'un système de réservation/connexion",
+      pricing: '1500€ - 5000€',
+    },
+    { name: 'Charte graphique', desc: "Création d'une charte graphique pour l'homogéïnité de l'application", pricing: '50€ - 200€' },
+    {
+      name: 'Designer',
+      desc: "Ajout d'un concepteur visuel de solutions de communication, réalisation d'une maquette visuelle combinant image et texte",
+      pricing: '100€ - 1000€',
+    },
+    { name: 'Langues', desc: 'Gestion des langues pour les écrans souhaités', pricing: '100€ - 500€ par langue' },
+    { name: 'Title', desc: 'Description', pricing: 'Pricing' },
+  ];
+
   private readonly destroy$ = new Subject<void>();
 
   constructor(private accountService: AccountService, private router: Router) {}
