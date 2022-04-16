894d30ba7d378f72115ced2616d4e01087deaf8a
MChaubet <mathieu.chaubet.travail@gmail.com>
2022-04-16 20:50:56 +0200

    [FRONT] : Changement mot de passe

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -3,40 +3,36 @@ import { RouterModule } from '@angular/router';
 
 import { errorRoute } from './layouts/error/error.route';
 import { navbarRoute } from './layouts/navbar/navbar.route';
-import { DEBUG_INFO_ENABLED } from 'app/app.constants';
 import { Authority } from 'app/config/authority.constants';
 
 import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(
-      [
-        {
-          path: 'admin',
-          data: { authorities: [Authority.ADMIN] },
-          canActivate: [UserRouteAccessService],
-          loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule),
-        },
-        {
-          path: 'account',
-          loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
-        },
-        {
-          path: 'login',
-          loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
-        },
-        {
-          path: 'modules',
-          data: { authorities: [Authority.USER] },
-          canActivate: [UserRouteAccessService],
-          loadChildren: () => import(`./modules/modules.module`).then(m => m.ModulesModule),
-        },
-        navbarRoute,
-        ...errorRoute,
-      ],
-      { enableTracing: DEBUG_INFO_ENABLED }
-    ),
+    RouterModule.forRoot([
+      {
+        path: 'admin',
+        data: { authorities: [Authority.ADMIN] },
+        canActivate: [UserRouteAccessService],
+        loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule),
+      },
+      {
+        path: 'account',
+        loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
+      },
+      {
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+      },
+      {
+        path: 'modules',
+        data: { authorities: [Authority.USER] },
+        canActivate: [UserRouteAccessService],
+        loadChildren: () => import(`./modules/modules.module`).then(m => m.ModulesModule),
+      },
+      navbarRoute,
+      ...errorRoute,
+    ]),
   ],
   exports: [RouterModule],
 })
