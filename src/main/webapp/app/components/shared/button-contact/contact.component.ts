37df43b2d542f63f6967b789e374122d525e9e80
MChaubet <mathieu.chaubet.travail@gmail.com>
2023-02-05 16:58:03 +0100

    [FRONT] : Début mise en place design pour modal contact

--- src/main/webapp/app/components/shared/button-contact/contact.component.ts
+++ src/main/webapp/app/components/shared/button-contact/contact.component.ts
@@ -14,30 +14,24 @@ export class ContactComponent {
 
   constructor(private modalService: NgbModal, private translateService: TranslateService) {
     this.questions = this.createQuestions();
-
-    console.log(this.questions);
   }
 
   openModalContact(content: TemplateRef<any>): void {
-    this.modalService.open(content, { centered: true, size: 'xl' });
+    this.modalService.open(content, { centered: true, size: 'xl', windowClass: 'modal-contact' });
   }
 
-  public answerQuestion(index: number, answer: string): void {
-    this.answers.push({ index, answer });
+  public answerQuestionButton(index: number, button: Button): void {
+    this.questions[index].buttons.forEach((b) => b.checked = false);
+    button.checked = true;
     this.nextQuestion();
   }
 
   public nextQuestion(): void {
     this.currentQuestionIdx++;
-    console.log(this.answers);
   }
 
   public previousQuestion(): void {
     this.currentQuestionIdx--;
-  }
-
-  public closeModal(): void {
-    this.modalService.dismissAll();
   }
 
   private createQuestions(): Question[] {
@@ -47,6 +41,9 @@ export class ContactComponent {
     const question1_Answer1 = this.translateService.instant('contact.questions.step1.answer1'); // WebSite
     const question1_Answer2 = this.translateService.instant('contact.questions.step1.answer2'); // Branding
     const question1_Answer3 = this.translateService.instant('contact.questions.step1.answer3'); // WebSite & Branding
+    const question1_Button1 = new Button(question1_Answer1);
+    const question1_Button2 = new Button(question1_Answer2);
+    const question1_Button3 = new Button(question1_Answer3);
 
     const question2 = this.translateService.instant('contact.questions.step2.question'); // Awesome! Can you give a few details about what you need?
     const question2_Input = this.translateService.instant('contact.questions.step2.input'); // Please share some project details
@@ -57,25 +54,33 @@ export class ContactComponent {
     const question3_Answer2 = this.translateService.instant('contact.questions.step3.answer2'); // 5K-10K
     const question3_Answer3 = this.translateService.instant('contact.questions.step3.answer3'); // 10K-15K
     const question3_Answer4 = this.translateService.instant('contact.questions.step3.answer4'); // +15K
+    const question3_Button1 = new Button(question3_Answer1);
+    const question3_Button2 = new Button(question3_Answer2);
+    const question3_Button3 = new Button(question3_Answer3);
+    const question3_Button4 = new Button(question3_Answer4);
 
     const question4 = this.translateService.instant('contact.questions.step4.question'); // And what’s your timeframe?
     const question4_Answer1 = this.translateService.instant('contact.questions.step4.answer1'); // Whenever
     const question4_Answer2 = this.translateService.instant('contact.questions.step4.answer2'); // Within 2 months
     const question4_Answer3 = this.translateService.instant('contact.questions.step4.answer3'); // Within 3 months
     const question4_Answer4 = this.translateService.instant('contact.questions.step4.answer4'); // Within 6 months
+    const question4_Button1 = new Button(question4_Answer1);
+    const question4_Button2 = new Button(question4_Answer2);
+    const question4_Button3 = new Button(question4_Answer3);
+    const question4_Button4 = new Button(question4_Answer4);
 
     const question5 = this.translateService.instant('contact.questions.step5.question'); // Lastly, could you please share a few more details?
     const question5_Input1 = this.translateService.instant('contact.questions.step5.input1'); // Your name
     const question5_Input2 = this.translateService.instant('contact.questions.step5.input2'); // Your email
     const question5_Input3 = this.translateService.instant('contact.questions.step5.input3'); // Any additional details? Existing website?
     const question5_InputDetail1 = new InputDetail('input', question5_Input1);
-    const question5_InputDetail2 = new InputDetail('input', question5_Input2);
+    const question5_InputDetail2 = new InputDetail('email', question5_Input2);
     const question5_InputDetail3 = new InputDetail('textarea', question5_Input3);
 
-    questions.push(new Question(question1, [question1_Answer1, question1_Answer2, question1_Answer3]));
+    questions.push(new Question(question1, [question1_Button1, question1_Button2, question1_Button3]));
     questions.push(new Question(question2, [], [question2_InputDetail1]));
-    questions.push(new Question(question3, [question3_Answer1, question3_Answer2, question3_Answer3, question3_Answer4]));
-    questions.push(new Question(question4, [question4_Answer1, question4_Answer2, question4_Answer3, question4_Answer4]));
+    questions.push(new Question(question3, [question3_Button1, question3_Button2, question3_Button3, question3_Button4]));
+    questions.push(new Question(question4, [question4_Button1, question4_Button2, question4_Button3, question4_Button4]));
     questions.push(new Question(question5, [], [question5_InputDetail1, question5_InputDetail2, question5_InputDetail3]));
 
     return questions;
@@ -83,9 +88,13 @@ export class ContactComponent {
 }
 
 export class Question {
-  constructor(public question: string, public answers: string[] = [], public inputs: InputDetail[] = []) {}
+  constructor(public question: string, public buttons: Button[] = [], public inputs: InputDetail[] = []) {}
+}
+
+export class Button {
+  constructor(public name: string, public checked = false) {}
 }
 
 export class InputDetail {
-  constructor(public type: string, public placeholder: string) {}
+  constructor(public type: string, public placeholder: string, public value?: string) {}
 }
